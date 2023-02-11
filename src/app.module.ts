55df9d3fca2a22ccaacf8268b163e0e435a3faf6
Your Name <gcoria1989@gmail.com>
2023-02-11 11:45:07 -0300

    agrega modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, OrdersModule, CheckoutModule, AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
