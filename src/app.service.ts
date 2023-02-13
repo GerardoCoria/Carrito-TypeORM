be1f37d0c92efbb4d6aee8a725fc2277f36f0773
Gerardo <gerardo_coria@solutionbox.com.ar>
2023-02-13 09:18:52 -0300

    modifica inicio

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): object {
-    return {message: 'Carrito de compras realizado en NestJs'}    ;
+    return {message: 'Carrito de compras realizado en NestJs, con TYypeORM'}    ;
   }
 }
