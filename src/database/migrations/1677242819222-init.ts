fcae98694de8d3f43de4f60e5c987c76b2927a12
Gerardo <gerardo_coria@solutionbox.com.ar>
2023-02-24 10:57:15 -0300

    agrega run migrations

--- src/database/migrations/1677242819222-init.ts
+++ src/database/migrations/1677242819222-init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class init1677185254857 implements MigrationInterface {
-    name = 'init1677185254857'
+export class init1677242819222 implements MigrationInterface {
+    name = 'init1677242819222'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "products" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "description" text NOT NULL, "price" integer NOT NULL, "stock" integer NOT NULL, "brand" character varying(255) NOT NULL, "expires" date NOT NULL, "batch" character varying(255) NOT NULL, CONSTRAINT "UQ_4c9fb58de893725258746385e16" UNIQUE ("name"), CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id"))`);
