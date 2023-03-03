c6232c4d7e24e91427b3c5ac594807008c807179
Gerardo <gerardo_coria@solutionbox.com.ar>
2023-03-03 17:12:56 -0300

    modifica customers

--- src/users/controllers/customer.controller.ts
+++ src/users/controllers/customer.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Put, Delete} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { CreateCustomerDto, UpdateCustomerDto } from '../dtos/customer.dto';
 import { CustomerService } from '../services/customer.service';
@@ -11,26 +11,31 @@ export class CustomersController {
   constructor(private customerService: CustomerService) {}
 
   @Get()
+  @ApiOperation({summary: ""})
   findAll() {
     return this.customerService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: ""})
   get(@Param('id',ParseIntPipe) id: number) {
     return this.customerService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({summary: ""})
   create(@Body() payload: CreateCustomerDto) {
     return this.customerService.create(payload);
   }
 
   @Put(':id')
+  @ApiOperation({summary: ""})
   update(@Param('id') id: number, @Body() payload: UpdateCustomerDto) {
     return this.customerService.update(id, payload);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: ""})
   remove(@Param('id') id: number) {
     return this.customerService.remove(id);
   }
